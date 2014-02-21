b378798120a9812a2e578e610bd7663d2a2864dc
Jon Friedman <jfriedman@motherjones.com>
2014-02-21 15:23:07 -0800

    make check_is_valid.js correctly handle relative paths

--- check_is_valid.js
+++ check_is_valid.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env nodejs
 
+var path = require('path');
 var argv = require('yargs').argv;
 var schemas = require('./schemas'); 
 
 var tv4s = schemas.tv4s;
 console.log(argv._);
-var json = require(argv._[0]);
+var json = require(path.resolve(process.cwd(), argv._[0]));
 console.log(tv4s.validateMultiple(json, schemas.articleSchema, true));
