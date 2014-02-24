d23c63060d7928045735cdc1c4b6edd7c6281292
Jon Friedman <jfriedman@motherjones.com>
2014-02-24 13:45:04 -0800

    added a usage message for check_is_valid.js

--- check_is_valid.js
+++ check_is_valid.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env nodejs
 
 var path = require('path');
-var argv = require('yargs').argv;
+var argv = require('yargs')
+    .usage('Validate a payload\'s schema\nUsage: $0 <schema name> <path to payload>')
+    .argv;
 var schemas = require('./schemas'); 
 
 var tv4s = schemas.tv4s;
-var json = require(path.resolve(process.cwd(), argv._[1]));
+var schema_type = argv._[0];
+var json_path = path.resolve(process.cwd(), argv._[1]);
+
+var json = require(json_path);
 console.log(tv4s.validateMultiple(json, argv._[0], true));
