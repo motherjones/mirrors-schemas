6e8fc2baa4c053bd85ce4ad7b91e1920c7ee8ee1
Jon Friedman <jfriedman@motherjones.com>
2014-02-24 15:32:33 -0800

    check_is_valid checks json structure before attempting to validate schema

--- check_is_valid.js
+++ check_is_valid.js
@@ -5,10 +5,19 @@ var argv = require('yargs')
     .usage('Validate a payload\'s schema\nUsage: $0 <schema name> <path to payload>')
     .argv;
 var schemas = require('./schemas'); 
+var exec = require('child_process').exec, child;
 
 var tv4s = schemas.tv4s;
 var schema_type = argv._[0];
 var json_path = path.resolve(process.cwd(), argv._[1]);
 
-var json = require(json_path);
-console.log(tv4s.validateMultiple(json, argv._[0], true));
+child = exec('cat ' + json_path + ' | json_pp', function (err, stdout, stderr) {
+    if ( err !== null ) {
+	console.error(err.Error);
+    } else {
+	var json = require(json_path);
+	console.log(tv4s.validateMultiple(json, schema_type, true));
+    }
+});
+
+
